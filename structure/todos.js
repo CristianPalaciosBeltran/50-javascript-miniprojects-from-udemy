4f93d3cafcf08b5815b8a6f05a1b92821dbfce12
Cristian Palacios Beltran <easyservice369@gmail.com>
2020-12-17 13:33:31 -0600

    Se creo proyecto de barra de progreso

--- structure/todos.js
+++ structure/todos.js
@@ -1,3 +1,11 @@
 //TODO: investigar para que sirve el overflow y pedirle a diego que te de una clase de eso
 
 //Links del proyecto https://github.com/bradtraversy/50projects50days
+//TODO: Que es box-sizing
+//TODO: Que es vh
+//TODO: overflow
+//TODO: background position x
+//TODO: que es position
+//TODO: que es transition 
+//TODO: entender position absolute
+//TODO: entender nth-of-type
